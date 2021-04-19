435d60c5692c1b5c152838cf42def74c0f440276
ZahraSaeed <80212819+ZahraSaeed@users.noreply.github.com>
2021-04-19 15:46:20 +0500

    Delete Lab 8 directory

--- Lab 8/Lab activity 1/L8AC1Runner.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class L8AC1Runner {
-    public static void main(String []args) {
-        Student s = new Student("s-09","Ahmed",246,"sp16-bcs-098",50);
-        s.display();
-    }
-}
